e2b735419cd2f99c8e3e890f611b1cea6d2483c8
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-09-27 18:37:55 +0500

    Initial Commit

---
+++ BreathEase/BreathEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathEaseApp.swift
+//  BreathEase
+//
+//  Created by Zaid Sheikh on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BreathEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
